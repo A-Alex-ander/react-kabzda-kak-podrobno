2b5224899a23f0c602496d1fb81b6ec2e470876a
A-Alex-ander <Dubrov.work@gmail.com>
2024-01-10 22:45:35 +0700

    07-uncontrolled-components

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {Rating} from "./components/Reting/Rating";
 import {Accordion} from "./components/Accordion/Accodion";
 import {OnOff} from "./components/OnOff/OnOff";
 import {UncontrolledAccordion} from "./components/Accordion/UncontrolledAccodion";
+import {UncontrolledRating} from "./components/Reting/UncontrolledRating";
 
 function App() {
     return (
@@ -15,12 +16,13 @@ function App() {
 
             {/*<Accordion titleValue={"Menu"} collapsed={true}/>*/}
             {/*<Accordion titleValue={"Users"} collapsed={false}/>*/}
-            <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
+            <UncontrolledRating/>
+            {/*<Rating value={0}/>*/}
+            {/*<Rating value={1}/>*/}
+            {/*<Rating value={2}/>*/}
+            {/*<Rating value={3}/>*/}
+            {/*<Rating value={4}/>*/}
+            {/*<Rating value={5}/>*/}
 
         </div>
     );
